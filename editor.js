c97692980044f2168272429660f5b2114e0dc702
trial-practice9 <theraunakyt77@gmail.com>
2025-09-24 12:28:06 +0530

    Update editor.js

--- editor.js
+++ editor.js
@@ -958,14 +958,17 @@ document.addEventListener("DOMContentLoaded", () => {
     setInterval(() => {
         editorState.showStatus('Auto-saved');
     }, 120000);
-});
 
-const textInputs = document.querySelectorAll('input[type="text"]');
+    const textInputs = document.querySelectorAll('input[type="text"]');
 
 textInputs.forEach(input => {
     if(input.placeholder === 'Link text...') {
         input.disabled = true;
     }
 });
+});
+
+
+
 
 
